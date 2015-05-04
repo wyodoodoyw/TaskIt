38061f4be841e657fa58301019998f875b015253
wyodoodoyw <mwood051@gmail.com>
2015-05-04 10:57:10 -0400

    hooked up imageview elements

--- LemonadeStand/LemonadeStand/ViewController.swift
+++ LemonadeStand/LemonadeStand/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var youHaveMoneyLabel: UILabel!
+    @IBOutlet weak var youHaveLemonsLabel: UILabel!
+    @IBOutlet weak var youHaveIceCubesLabel: UILabel!
+    
+    @IBOutlet weak var purchaseLemonsLabel: UILabel!
+    @IBOutlet weak var purchaseIceCubesLabel: UILabel!
+    
+    @IBOutlet weak var mixLemonsLabel: UILabel!
+    @IBOutlet weak var mixIceCubesLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +32,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func increasePurchaseLemons(sender: UIButton) {
+    }
+   
+    @IBAction func decreasePurchaseLemons(sender: UIButton) {
+    }
+    
+    @IBAction func increasePurchaseIceCubes(sender: UIButton) {
+    }
+    
+    @IBAction func decreasePurchaseIceCubes(sender: UIButton) {
+    }
+    
+    @IBAction func increaseMixLemons(sender: UIButton) {
+    }
+    
+    @IBAction func decreaseMixLemons(sender: UIButton) {
+    }
+    
+    @IBAction func increaseMixIceCubes(sender: UIButton) {
+    }
+    
+    @IBAction func decreaseMixIceCubes(sender: UIButton) {
+    }
+    
+    @IBAction func startDayButtonPressed(sender: UIButton) {
+    }
+    
 }
 
